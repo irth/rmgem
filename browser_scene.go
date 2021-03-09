b3d0309c70a77aa9a8da67fbc28f01977121f485
Wojciech Kwolek <me@irth.pl>
2021-03-09 20:16:50 +0100

    rough pagination

--- browser_scene.go
+++ browser_scene.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"git.sr.ht/~adnano/go-gemini"
 	"github.com/irth/go-simple"
 	ui "github.com/irth/go-simple"
+	"github.com/mitchellh/go-wordwrap"
 )
 
 type BrowserScene struct {
-	r       *RMGem
-	url     string
-	gemtext gemini.Text
+	r           *RMGem
+	url         string
+	gemtext     gemini.Text
+	pages       []gemini.Text
+	currentPage int
+	pageHeight  int
 }
 
 func NewBrowserScene(r *RMGem, url string) simple.Scene {
 	b := &BrowserScene{
-		r, url, nil,
+		r:           r,
+		url:         url,
+		gemtext:     nil,
+		pages:       nil,
+		currentPage: 0,
+		pageHeight:  30,
 	}
 	return b
 }
 
 func (b *BrowserScene) Render() (ui.Widget, error) {
+	var prev simple.Widget
+	var next simple.Widget
+	if b.currentPage > 0 {
+		prev = ui.Button(
+			"prev",
+			ui.Pos(ui.Abs(50), ui.Abs(b.r.simple.ScreenHeight()-90), ui.Abs(150), ui.Abs(100)),
+			"[prev]",
+			func(a *ui.App, btn *ui.ButtonWidget) error { b.previousPage(); return nil },
+		)
+	}
+	if b.currentPage < len(b.pages)-1 {
+		next = ui.Button(
+			"next",
+			ui.Pos(ui.Abs(b.r.simple.ScreenWidth()-50-120), ui.Abs(b.r.simple.ScreenHeight()-90), ui.Abs(150), ui.Abs(100)),
+			"[next]",
+			func(a *ui.App, btn *ui.ButtonWidget) error { b.nextPage(); return nil },
+		)
+	}
+
 	return ui.WidgetList{
 		ui.Justify(ui.Left),
 		ui.FontSize(42),
@@ -50,24 +79,99 @@ func (b *BrowserScene) Render() (ui.Widget, error) {
 		// Displaying user generated content using paragraph is unsafe, because AFAIK ] cannot be escaped.
 		// Newlines should not be a problem, as each newline in Gemini generates a new widget.
 		b.renderSite(),
+		ui.Label(
+			ui.Pos(ui.Abs(0), ui.Abs(b.r.simple.ScreenHeight()-140), ui.Abs(b.r.simple.ScreenWidth()), ui.Abs(55)),
+			"______________________________________________________________________________________",
+		),
+		ui.FontSize(40),
+		prev,
+		next,
 	}, nil
 }
 
 func (b *BrowserScene) fetch() error {
 	var err error
+	b.currentPage = 0
 	b.gemtext, err = Fetch(b.url)
+	if err != nil {
+		b.gemtext = nil
+		b.pages = nil
+		return err
+	}
+	b.pages = b.splitSite(b.gemtext)
 	return err
 }
 
+func (b *BrowserScene) nextPage() {
+	if b.currentPage < len(b.pages)-1 {
+		b.currentPage++
+	}
+}
+
+func (b *BrowserScene) previousPage() {
+	if b.currentPage > 0 {
+		b.currentPage--
+	}
+}
+
+func (b *BrowserScene) estimateHeight(l gemini.Line) int {
+	switch l := l.(type) {
+	case gemini.LineText:
+		if l.String() == "" {
+			return 0
+		}
+		wrapped := wordwrap.WrapString(l.String(), 71) // 71 - line wrap length, checked experimentally
+		return len(strings.Split(wrapped, "\n"))
+	case gemini.LineHeading1, gemini.LineHeading2, gemini.LineHeading3:
+		return 2
+	default:
+		return 1
+	}
+}
+
+func (b *BrowserScene) splitSite(site gemini.Text) []gemini.Text {
+	pages := []gemini.Text{}
+	var page gemini.Text
+	height := 0
+	for _, line := range site {
+		lineHeight := b.estimateHeight(line)
+		height += lineHeight
+		if height > b.pageHeight {
+			// if the element won't fit, start a new page
+			pages = append(pages, page)
+			page = gemini.Text{}
+			height = 0
+		}
+		if lineHeight > b.pageHeight {
+			// if the element is too big to fit on a single page,
+			// give it a dedicated page and hope for the best
+			// TODO: solve this better
+			pages = append(pages, gemini.Text{line})
+			continue
+		}
+		page = append(page, line)
+	}
+	pages = append(pages, page)
+	return pages
+}
+
+func (b *BrowserScene) getCurrentPage() gemini.Text {
+	if b.pages == nil {
+		return nil
+	}
+	return b.pages[b.currentPage]
+}
+
 func (b *BrowserScene) renderSite() simple.Widget {
-	pos := ui.Pos(ui.Abs(100), ui.Step, ui.Abs(b.r.simple.ScreenWidth()-200), ui.Abs(60))
+	pos := ui.Pos(ui.Abs(100), ui.Step, ui.Abs(b.r.simple.ScreenWidth()-200), ui.Abs(55))
 	wl := ui.WidgetList{
 		ui.FontSize(32),
 		ui.Label(pos, " "),
 	}
+
 	wasButton := false
 	isButton := false
-	for idx, line := range b.gemtext {
+	for idx, line := range b.getCurrentPage() {
 		wasButton = isButton
 		isButton = false
 		var widget simple.Widget
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	git.sr.ht/~adnano/go-gemini v0.1.19 // indirect
-	github.com/irth/go-simple v0.0.0-20210309172018-5cc989c2e6e5
+	github.com/irth/go-simple v0.0.0-20210309191312-fedb235a168c
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 )
